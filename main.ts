966517e6b6a9bce875e760e6d63abb4bdb4bd999
Daniel McGillick <56147357+DMacMakes@users.noreply.github.com>
2023-07-10 23:41:20 +1000

    Appears to work!

--- main.ts
+++ main.ts
@@ -1,15 +1,8 @@
-EMF_Gamepad.onEMFButtonEvent(EMF_Button.L_BUTTON, Button_State.DOWN, function () {
-    serial.writeLine("on L DOWN")
+EMF_Gamepad.onEMFButtonEvent(EMF_Button.L_BUTTON, EMFButton_Event.BUTTON_UP, function () {
+    serial.writeLine("test app: on L is Up")
 })
-EMF_Gamepad.onEMFButtonEvent(EMF_Button.R_BUTTON, Button_State.DOWN, function () {
-    serial.writeLine("on L UP")
-})
-EMF_Gamepad.onStickEvent(Stick_Id.STICK_LEFT, Stick_Event.CHANGED_DIR, function () {
-    radio.sendValue("change direction", 0)
-    serial.writeLine("" + (EMF_Gamepad.Stick_direction(Stick_Id.STICK_LEFT)))
+EMF_Gamepad.onEMFButtonEvent(EMF_Button.L_BUTTON, EMFButton_Event.BUTTON_DOWN, function () {
+    serial.writeLine("test app: on L is Down")
 })
 radio.setGroup(20)
-serial.writeValue("Left stick direction on start:", EMF_Gamepad.Stick_direction(Stick_Id.STICK_LEFT))
-basic.forever(function () {
-	
-})
+serial.writeLine("test app: waking up")
