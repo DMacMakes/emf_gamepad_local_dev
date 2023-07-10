66b20cd32411f79086bdbd56bd3da0be82a6cf6a
Daniel McGillick <56147357+DMacMakes@users.noreply.github.com>
2023-07-10 23:11:51 +1000

    attempts at reading and reporting buttons

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
-EMF_Gamepad.onEvent(Stick_Id.STICK_LEFT, Stick_Event.CHANGED_DIR, function () {
-    radio.sendValue("change direction", EMF_Gamepad.Get_left_stick_direction())
+EMF_Gamepad.onEMFButtonEvent(EMF_Button.L_BUTTON, Button_State.DOWN, function () {
+    serial.writeLine("on L DOWN")
+})
+EMF_Gamepad.onEMFButtonEvent(EMF_Button.R_BUTTON, Button_State.DOWN, function () {
+    serial.writeLine("on L UP")
+})
+EMF_Gamepad.onStickEvent(Stick_Id.STICK_LEFT, Stick_Event.CHANGED_DIR, function () {
+    radio.sendValue("change direction", 0)
+    serial.writeLine("" + (EMF_Gamepad.Stick_direction(Stick_Id.STICK_LEFT)))
 })
 radio.setGroup(20)
+serial.writeValue("Left stick direction on start:", EMF_Gamepad.Stick_direction(Stick_Id.STICK_LEFT))
 basic.forever(function () {
 	
 })
